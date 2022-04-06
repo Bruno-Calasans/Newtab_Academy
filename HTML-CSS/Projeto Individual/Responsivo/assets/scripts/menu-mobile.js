dc85287639b4803255438b12837462e8aedf50db
Houllp <83395524+Houllp@users.noreply.github.com>
2022-04-05 22:34:37 -0300

    projeto individual - final(update 5)

--- HTML-CSS/Projeto Individual/Responsivo/assets/scripts/menu-mobile.js
+++ HTML-CSS/Projeto Individual/Responsivo/assets/scripts/menu-mobile.js
@@ -1,14 +1,12 @@
 
 
-    // pegando os elementos
-    const closeBtn = document.getElementById('closeBtn')
-    const menuToggle = document.getElementById('menu-toggle')
-    const menu = document.getElementById('menu')
-    
-    // adicionando eventos
-    menuToggle.onclick = openSideMenu
-    closeBtn.onclick = closeSideMenu
+    // pegando os elementos principais
+    const closeBtn = document.getElementById('closeBtn') // botão de fechar
+    const menuToggle = document.getElementById('menu-toggle') // menu mobile
+    const menu = document.getElementById('menu') // menu 
+    const separador = document.querySelector('.separador')
 
+    // abre o menu mobile lateral
     function openSideMenu(){
 
         // exibindo o menu
@@ -18,20 +16,21 @@
         const lista = document.querySelector('.menu-list')
         if(lista) lista.className = 'menu-list-toggle'
 
+        // ocultando o separador de lista
+        separador.setAttribute('style', 'display: none')
+
         // alterando a classe dos items da lista
         const items = document.querySelectorAll('.list-item')
         if(items){
-            for(let item of items) {item.className = 'list-item-toggle'}
+            for(let item of items) item.className = 'list-item-toggle'
         }
 
         // exibindo o botão de fechar
         closeBtn.setAttribute('style', 'display: initial')
     }
 
+    // fecha o menu mobile lateral
     function closeSideMenu(){
-
-        // verificando largura da viewport
-        const larguraAtual = document.body.clientWidth
 
         // ocultando o menu
         menu.removeAttribute('style')
@@ -39,6 +38,9 @@
         // alterando a classe da lista do menu
         const lista = document.querySelector('.menu-list-toggle')
         if(lista) lista.className = 'menu-list'
+
+        // mostrando o separador de lista
+        separador.removeAttribute('style')
 
         // alterando a classe de cada item da lista
         const items = document.querySelectorAll('.list-item-toggle')
@@ -50,7 +52,11 @@
         closeBtn.removeAttribute('style')
     }
 
-    // fechando menu-toggle ao clicar fora dele
+    // adicionando eventos
+    menuToggle.onclick = openSideMenu
+    closeBtn.onclick = closeSideMenu
+
+    // fechando menu mobile ao clicar fora do próprio menu
     document.body.onclick = (e) => {
     if(e.target.id != 'menu-toggle'
     && e.target.className != 'menu-list-toggle') closeSideMenu()}
